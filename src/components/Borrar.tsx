60f4aa40de1812f69698e4daef72033f49789a15
Angel R <angelss1194@gmail.com>
2024-01-08 20:50:38 -0600

    Implement

---
+++ src/components/Borrar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Borrar = () => {
+  return (
+    <div>Borrar</div>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,8 @@ export const Navbar = () => {
             <div className={`md:hidden bg-cyan-900 text-white fixed w-full overflow-y-auto py-8 px-5
                 duration-500 ${open ? "left-0" : "left-[-100%]"}`}>
 
-                    <h1>Hola</h1>
+                    <h1>Hola feo</h1>
+                    h4,Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, dolore! Praesentium corrupti adipisci optio rem consequatur. Eos voluptatibus dignissimos ratione unde provident architecto voluptatum aliquid! Error suscipit tenetur sint adipisci atque reiciendis magnam aliquam nostrum quibusdam natus quisquam voluptate amet laudantium eum similique ad dolorem magni totam, provident necessitatibus, odit, sunt voluptatem! Cum, at laudantium, cumque aliquam nesciunt natus molestiae, dolorum minus exercitationem hic sunt nihil vero eum nostrum aperiam possimus asperiores tenetur vel in voluptas reprehenderit et odit dignissimos quibusdam! Voluptatem non quia quibusdam quae nulla? Sequi velit ea accusamus quisquam dolorum omnis doloribus cupiditate, eligendi quibusdam eaque minus!
                 </div>
         </nav>
   
